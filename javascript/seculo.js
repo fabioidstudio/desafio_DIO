4139b386d0c531217bd46e6e1939a43cd8057f0b
Fabio <fabio.idstudio@gmail.com>
2021-09-27 18:56:26 -0300

    Modificando função PHP por JS

---
+++ javascript/seculo.js
@@ -0,0 +1,8 @@
+
+	//FUNÇÃO SÉCULO-ANO
+	//Chamando a função com um ano válido, ela retorna o século ao qual o ano pertence
+	
+	function seculo(ano){
+		seculo = ceil(ano/100);
+		alert("Ano ano pertence ao século seculo");
+	}
