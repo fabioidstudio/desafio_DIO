a4300b95567a770b34bc4e4ae99024b3d7e10770
Fabio <fabio.idstudio@gmail.com>
2021-09-27 23:00:16 -0300

    Arquivos funcionais enviados

--- javascript/seculo.js
+++ javascript/seculo.js
@@ -2,7 +2,8 @@
 	//FUNÇÃO SÉCULO-ANO
 	//Chamando a função com um ano válido, ela retorna o século ao qual o ano pertence
 	
-	function seculo(ano){
-		seculo = ceil(ano/100);
-		alert("Ano ano pertence ao século seculo");
+	function seculo(){
+		var escolhido = document.querySelector("#ano").value;
+		var resultado = Math.ceil(escolhido/100);
+		document.querySelector("#valor").innerHTML = "O ano " +escolhido+ " pertence ao século " +resultado;
 	}
